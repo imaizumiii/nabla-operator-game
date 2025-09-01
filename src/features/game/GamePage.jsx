2f115089e38107a6ecb4aa296a47a69339d4e9ee
imaizumiii <131867722+imaizumiii@users.noreply.github.com>
2025-09-01 18:00:43 +0900

    feat: create cards.js

--- src/features/game/GamePage.jsx
+++ src/features/game/GamePage.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 export default function GamePage() {
-  return (
-    <div style ={{ padding: "2rem"}}>
-      <h1>Game Page (Dummy)</h1>
-      <p>ここに計算機を使ったゲームを実装予定です。</p>
-    </div>
-  )
+  const [status, setStatus] = useState("idle"); //idele or started
+
+  if (status === "idle") {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>Game Page</h1>
+        <button onClick={() => setStatus("started")}>ゲームスタート</button>
+      </div>
+    );
+  }
+  if (status === "started"){
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>ゲーム中</h1>
+        <p>ここにゲームロジックを追加</p>
+      </div>
+    );
+  }
+
+  return null;
 }
