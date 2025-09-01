2797d4f3c40d697229d7082c47791cd1ecd13605
imaizumiii <131867722+imaizumiii@users.noreply.github.com>
2025-09-01 15:00:36 +0900

    feat: ダミーページを確保

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 //src/App.jsx
 import "./App.css";
 import CalculatorPage from "./features/calclator/CalculatorPage.jsx";
+import { BrowserRouter as Router, Routes, Route, Link }  from "react-router-dom";
+import GamePage from "./features/game/GamePage.jsx";
 
 export default function App() {
-  return <CalculatorPage />;
+  return (
+    <Router>
+      <nav style={{ padding: "1rem", borderBottom: "1px solid #ddd" }}>
+        <Link to="/">Calculator</Link> | <Link to="/game">Game</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<CalculatorPage />} />
+        <Route path="/game" element={<GamePage />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ src/features/game/GamePage.jsx
@@ -0,0 +1,8 @@
+export default function GamePage() {
+  return (
+    <div style ={{ padding: "2rem"}}>
+      <h1>Game Page (Dummy)</h1>
+      <p>ここに計算機を使ったゲームを実装予定です。</p>
+    </div>
+  )
+}
